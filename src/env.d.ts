5b5168f3a82a3b4e9f1014c4da388426f3592200
Leo-paul <leopaulmartin@hotmail.fr>
2022-08-25 23:07:29 +0200

    add env variables config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+// Use this file to type your environment variables!
+// See https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript for more information
+
+interface ImportMetaEnv {
+	readonly NASA_API: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
